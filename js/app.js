9cd558351a02e7603d9c1b95851f3dd17bdaea1c
Aleksandar Takač <Tacke@Aleksandars-MacBook-Air.local>
2022-03-07 21:29:15 +0100

    123

--- js/app.js
+++ js/app.js
@@ -14,3 +14,9 @@ burger.addEventListener('click', function () {
     this.classList.toggle('active');
     alterNav.classList.toggle('open');
   });
+
+const tl = gsap.timeline({defaults:{duration: 0.75, ease: 'power1.out'}})
+
+// gsap.set('.hero__txt1',{display: block})
+
+tl.fromTo('.hero__txt1',{x: -10, opacity: 0}, {x: 0, opacity: 1})
